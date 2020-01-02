c26d9c241f012b6d5d8b57561e84fc2988876e2d
thatkstho <othmanehachim@gmail.com>
2020-01-02 06:46:03 +0100

    I think we have to main frame for the code execution.

--- VM/src/ft_battlegrounds.c
+++ VM/src/ft_battlegrounds.c
@@ -6,7 +6,7 @@
 /*   By: ohachim <ohachim@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/31 01:34:58 by ohachim           #+#    #+#             */
-/*   Updated: 2019/12/31 04:11:17 by ohachim          ###   ########.fr       */
+/*   Updated: 2020/01/01 21:50:20 by ohachim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,22 @@ static void	ft_get_op(t_global *global_data)
 static int	ft_enough_processes(t_global *global_data)
 {
 	t_process *temp_processes;
+	int		alive;
 
+	alive = 0;
 	temp_process = global_data->processes;
 	while (temp_process)
 	{
 		if (temp_process->alive)
-			return (1);
+			alive++;
 		temp_process = temp_process->next;
 	}
-	return (0);
+	return (alive);
 }
 
 void	ft_battlegrounds(t_global *global_data)
 {
-	while (global_data->cycles_to_die && ft_enough_processes(global_data))
+	while (global_data->cycles_to_die > 0 && ft_enough_processes(global_data) > 1)
 	{
 		global_data->cycles_since_start = 0;
 		while (global_data->cycle_since_start < global_data->cycles_to_die)
@@ -48,9 +50,13 @@ void	ft_battlegrounds(t_global *global_data)
 		 * Maybe count lives here, maybe we count lives while executing code.
 		 * Kill processes that didn't declare live.
 		 * Give new values to variables.
+		 * Remeber player who last declared to be alive.
 		 */
 		if (global_data->number_of_checks >= MAX_CHECKS)
+		{
 			global_data->cycles_to_die -= CYCLE_DELTA;
+			global_data->number_of_checks = 0;
+		}
 		if (global_data->number_lives_declared >= NBR_LIVE)
 		{
 			global_data->cycles_to_die -= CYCLE_DELTA;
@@ -58,4 +64,8 @@ void	ft_battlegrounds(t_global *global_data)
 		}
 		global_data->number_lives_declared = 0;
 	}
+	/*
+	 * We should keep executing code after cycles_to_die is less than or equal to zero, but this time we do the verification after every cycle.
+	 * Or maybe we just stop after cycle_to_die reaches 0.
+	 */
 }
